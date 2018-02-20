83f40b22397ce8ca41e077a3e24f9e36453bee45
Travis Clarkson <tk1667@gmail.com>
2018-02-20 03:11:58 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort {|x,y| y<=>x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array.insert(2,array.slice!(1))
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |string|
+    new = string.split('')
+    new[2] = "$"
+    kesha << new.join
+  end
+  kesha
+end
+
+def find_a(array)
+  array.select {|i| i < "b"}
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect {|element, index| index == 1 ? element : element << "s"}
+end
